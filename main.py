9649a824d42dfacf1158344fa34236d9af4ede0c
duduomena1 <duduomena.ec@icloud.com>
2025-04-23 19:58:10 -0300

    Update page configurations and file references

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 # Define the pages
 main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
-page_2 = st.Page("cadastro.py", title="Cadastro de Produtos", icon="🗃️")
-page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
+page_2 = st.Page("register.py", title="Register products", icon="🗃️")
+page_3 = st.Page("users.py", title="Users", icon="👤")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
