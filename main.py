f6464f4cb45ec83a828a2a007479a5f1e95e4ac1
duduomena1 <duduomena.ec@icloud.com>
2025-04-23 20:13:39 -0300

    Update 'Users' page title to 'Register Users'

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Define the pages
 main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
 page_2 = st.Page("register.py", title="Register products", icon="🗃️")
-page_3 = st.Page("users.py", title="Users", icon="👤")
+page_3 = st.Page("users.py", title="Register Users", icon="👤")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
